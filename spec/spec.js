0b815ebae9dc372f9106aefa02031c59bb524bf5
Guy Pursey <guypursey@gmail.com>
2023-09-19 11:09:08 +0100

    Split proposed examples and requirement tests into their own files.
    I wanted a bit more focus when it came to writing test requirements
    and proposing possible conventions.
    
    I also wanted to see if I could check coverage... Firstly with NYC
    but if not then perhaps by writing some custom functions to check
    if all values in the test requirements exist in the proposal (and
    highlight any gaps) and to check if all values in the proposal
    are being tested. Two-way coverage check.

--- spec/spec.js
+++ spec/spec.js
@@ -1,66 +1,17 @@
-const chai = require("chai").should();
-//const diarise = require("../src/index.js");
-
-// TODO: Try splitting out into separate proposal/examples JSON file?
+const prop = require("./prop.json");
+const reqs = require("./reqs.json");
 // TODO: Check NYC coverage after split to see if coverage of JSON is also handled
-let testDictionary = {
-    "1900s (century)":          "20C",
-    "1900s (decade)":           "1900s",
-    "21st century":             "21C",
-    "2019":                     "2019",
-    "2020":                     "2020",
-    "2020s (decade)":           "2020s",
-    "2021":                     "2021",
-    "2023":                     "2023",
-    "Winter or Q1 2023":        "2023Q1",
-    "January 2023":             "202301",
-    "1st January 2023":         "20230101",
-    "1st January 2023 + note":  "20230101: This is a note",
-    "Noon on 1st January 2023": "202301011200",
-    "2nd January 2023":         "20230102",
-    "March 2023":               "202303",
-    "Spring or Q2 2023":        "2023Q2",
-    "April 2023":               "202304",
-    "Summer or Q3 2023":        "2023Q3",
-    "June 2023":                "202306"
-};
 
+const chai = require("chai").should();
 lookupDate = key => ({
         "key": key,
-        "val": testDictionary[key]
+        "val": prop[key]
     });
     // TODO: add some error or undefined logging here, perhaps via monad
 
 describe("Checking ordering of plain-text dates", function () {
     // TODO: Try splitting out into separate suites (basic requirements, advanced requirements)
-    // TODO: Try splitting out into separate file for coverage purposes?
-    const tests = [
-        {
-            test: "where one day should follow another",
-            input: [ "2nd January 2023", "1st January 2023" ],
-            expected: [ "1st January 2023", "2nd January 2023" ]
-        },
-        {
-            test: "with a month representation and a day representation",
-            input: [ "1st January 2023", "January 2023" ],
-            expected: [ "January 2023", "1st January 2023" ]
-        },
-        {
-            test: "with a year-resoluton date and a month-resolution date",
-            input: [ "January 2023", "2023" ],
-            expected: [ "2023", "January 2023" ]
-        },
-        {
-            test: "with a decade-resolution date and three year-resolution dates straddling it",
-            input: [ "2020s (decade)", "2019", "2020", "2021" ],
-            expected: [ "2019", "2020s (decade)", "2020", "2021" ]
-        },
-        {
-            test: "with a century-resolution date and decade-resolution date",
-            input: [ "1900s (decade)", "1990s (century)" ],
-            expected: [ "1900s (century)", "1900s (decade)" ]
-        }
-    ];
+    const tests = reqs;
     tests.forEach(({test, input, expected}) => {
         describe(test, function () {
             const processedInput = input.map(x => lookupDate(x))
